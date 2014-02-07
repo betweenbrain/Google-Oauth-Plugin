cc26909fbd447ebf6e0d53faed944e8157630a54
Matt Thomas <matt@betweenbrain.com>
2014-02-07 04:48:17 -0500

    Base POC

---
+++ elements/diagnostic.php
@@ -0,0 +1,134 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       diagnostic.php
+ * Created    2/12/13 11:59 AM
+ * Author     Matt Thomas
+ * Website    http://betweenbrain.com
+ * Email      matt@betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2012 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v3 or later
+ */
+class JElementDiagnostic extends JElement
+{
+
+	/**
+	 * Element name
+	 *
+	 * @access       protected
+	 * @var          string
+	 */
+	var $_name = 'Diagnostic';
+
+	/**
+	 * @param $name          unique name of the parameter, from the name argument.
+	 * @param $value         current value of the parameter.
+	 * @param $node          JSimpleXMLElement object representing the <param> element.
+	 * @param $control_name  parameter type from the type argument (eg. 'category' or 'newparm')
+	 *
+	 * @return bool|mixed
+	 */
+
+	function fetchElement($name, $value, &$node, $control_name)
+	{
+
+		$element    = $node->attributes('element');
+		$cacheDir   = $node->attributes('cachedir');
+		$cacheDir   = JPATH_SITE . '/cache/' . $cacheDir;
+		$cacheFiles = $node->attributes('cachefiles');
+
+		// Fetch parameters via database query
+		$db  = JFactory::getDBO();
+		$sql = 'SELECT ' . $db->nameQuote('params') .
+			' FROM ' . $db->nameQuote('#__plugins') .
+			' WHERE ' . $db->nameQuote('element') . ' = ' . $db->quote($element);
+		$db->setQuery($sql);
+		$params = $db->loadResult();
+		$params = parse_ini_string($params);
+
+		// Initialize variables
+		$result   = null;
+		$messages = null;
+		$errors   = null;
+
+		if ($params['showDiagnostic'] == 1)
+		{
+
+			// Check caching parameter
+			if (isset($params['cache']) && $params['cache'] == 1)
+			{
+				$messages[] = ucfirst($element) . 'caching parameter is enabled.';
+			}
+
+			// Check cache files
+			$cacheFiles = explode(',', $cacheFiles);
+			foreach ($cacheFiles as $cacheFile)
+			{
+				$cacheFile = $cacheDir . '/' . $cacheFile;
+
+				if (file_exists($cacheFile))
+				{
+					$cacheAge   = date("F d Y H:i:s", filemtime($cacheFile));
+					$messages[] = "Cache file at $cacheFile exists.";
+					$messages[] = "Cache file was created $cacheAge.";
+				}
+				else
+				{
+					$errors[] = "Cache file at $cacheFile does not exist!";
+				}
+			}
+
+			if (isset($params['cacheage']))
+			{
+				$messages[] = "Cache lifetime is " . $params['cacheage'] . " minute(s).<br/>";
+			}
+
+			if (is_dir($cacheDir))
+			{
+				$messages[] = "Cache directory at $cacheDir exists.";
+				if (is_writable($cacheDir))
+				{
+					$messages[] = "Cache directory at $cacheDir is writable.";
+				}
+			}
+			else
+			{
+				$errors[] = "Cache directory at $cacheDir does not exist!";
+				if (!is_writable($cacheDir))
+				{
+					$errors[] = "Cache directory at $cacheDir is not writable!";
+				}
+			}
+
+			if ($messages[0])
+			{
+				$result .= '<dl id="system-message"><dt>Information</dt><dd class="message fade"><ul>';
+				foreach ($messages as $message)
+				{
+					$result .= '<li>' . $message . '</li>';
+				}
+				$result .= '</ul></dd></dl>';
+			}
+
+			if ($errors[0])
+			{
+				$result .= '<dl id="system-message"><dt>Errors</dt><dd class="error message fade"><ul>';
+				foreach ($errors as $error)
+				{
+					$result .= '<li>' . $error . '</li>';
+				}
+				$result .= '</ul></dd></dl>';
+			}
+
+			if ($result)
+			{
+				return print_r($result, false);
+			}
+
+			return false;
+		}
+
+		return false;
+	}
+}
---
+++ googleoauth.php
@@ -0,0 +1,12 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * File       googleoauth.php
+ * Created    2/7/14 3:40 AM 
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2014 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v3 or later
+ */
+ 
+ 
